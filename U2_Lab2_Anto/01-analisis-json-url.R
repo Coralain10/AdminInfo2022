c705f7ff6748ac2be04eedd0220afa4c817e9f2f
Coralain <carolain.antoc@gmail.com>
2022-04-21 12:32:05 -0500

    Unidad 2: Laboratorio 2

--- U2_Lab2_Anto/01-analisis-json-url.R
+++ U2_Lab2_Anto/01-analisis-json-url.R
@@ -2,10 +2,13 @@ install.packages("jsonlite")
 library(jsonlite)
 usd.url <- "http://www.floatrates.com/daily/usd.json"
 usd.pob <- fromJSON(usd.url)
+
+# Creando dataframe vacío
 usd.colnames <- names(usd.pob[[1]])
 usd.data <- data.frame(matrix(ncol = length(usd.colnames), nrow = 0))
 colnames(usd.data) <- usd.colnames
 
+# insertando los datos en el dataframe
 for (abr in names(usd.pob)) {
   dat <- usd.pob[[abr]]
   usd.data[nrow(usd.data) + 1,] = dat
